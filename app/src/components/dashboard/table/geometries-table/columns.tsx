51842e10d13c68311ff473ffba3f125b4769277d
Eray Ates <eray.ates@priviasecurity.com>
2024-09-14 05:43:58 +0300

    update: Table column widths rearranged.

--- app/src/components/dashboard/table/geometries-table/columns.tsx
+++ app/src/components/dashboard/table/geometries-table/columns.tsx
@@ -8,17 +8,25 @@ export const GeometriesTableColumns: ColumnDef<IGeometry>[] = [
     header: "User ID",
     cell: ({ getValue }) => {
       const appUserId = getValue() as string;
-      return <div className="truncate w-[200px]">{appUserId}</div>;
+      return <div className="truncate w-[350px]">{appUserId}</div>;
     },
   },
 
   {
     accessorKey: "name",
     header: "Name",
+    cell: ({ getValue }) => {
+      const name = getValue() as string;
+      return <div className="truncate w-[200px]">{name}</div>;
+    },
   },
   {
     accessorKey: "wkt",
     header: "WKT",
+    cell: ({ getValue }) => {
+      const wkt = getValue() as string;
+      return <div className="truncate w-[350px]">{wkt}</div>;
+    },
   },
 
   {
--- app/src/components/dashboard/table/users-table/columns.tsx
+++ app/src/components/dashboard/table/users-table/columns.tsx
@@ -9,24 +9,36 @@ export const UserTableColumns: ColumnDef<IUser>[] = [
     header: "ID",
     cell: ({ getValue }) => {
       const userId = getValue() as string;
-      return <div className="truncate w-[200px]">{userId}</div>;
+      return <div className="truncate w-[350px]">{userId}</div>;
     },
   },
 
   {
     accessorKey: "userName",
     header: "Username",
+    cell: ({ getValue }) => {
+      const userName = getValue() as string;
+      return <div className="truncate w-[200px]">{userName}</div>;
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
+    cell: ({ getValue }) => {
+      const email = getValue() as string;
+      return <div className="truncate w-[350px]">{email}</div>;
+    },
   },
 
   {
     id: "actions",
     header: "Actions",
     cell: ({ row }) => {
-      const rowData = row.original as { id: string; userName: string; email: string };
+      const rowData = row.original as {
+        id: string;
+        userName: string;
+        email: string;
+      };
       return <UsersTableActions rowData={rowData} />;
     },
   },
